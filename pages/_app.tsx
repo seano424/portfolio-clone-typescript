e4f7f368d7b51e36ae9e6108efb1e82ebc89160b
Sean OReilly <soreilly424@gmail.com>
2022-06-23 10:44:49 -0500

    removed animatepresence from app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,12 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
-import { AnimatePresence } from 'framer-motion'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <AnimatePresence>
-      <ThemeProvider attribute="class">
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </AnimatePresence>
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
   )
 }
 
